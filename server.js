419dceaab448226bf0a5d3eaddde2b641b1dad36
kaamwallay <thekaamwallay@gmail.com>
2022-07-09 02:27:35 +0500

    Setting up circle ci

---
+++ server.js
@@ -0,0 +1,17 @@
+/**
+ * Testing server for application testing. 
+ */
+
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+app.listen(port);
+
+console.log('Application listning on port ' + port);
